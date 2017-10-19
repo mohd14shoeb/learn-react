2a6639c6d797a108cf19131b1697792a8395bd1c
Sang Dang <sangdth@gmail.com>
2017-10-19 12:14:11 +0300

    Fix env vars

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,6 @@
-module.exports = {
-  googleClientID: '787828111754-voglukjmj42r5sagbhsppu192628bin8.apps.googleusercontent.com',
-  googleClientSecret: 'EPHG6mgZctNoDT4mU3HRARme',
-  mongoURI: 'mongodb://sangdth:lorem123@ds119685.mlab.com:19685/emaily-dev',
-  cookieKey: 'asfas67890asdfsdf2jhgjlptroeiuadfasdfa'
-};
+// key.js where we detect which env file to get 
+if (process.env.NODE_ENV === 'production') {
+  module.exports = require('./pro.js');
+} else {
+  module.exports = require('./dev.js');
+}
